57fbeaa5938ef46c193f1d3b93983ab3559d4019
Aaron Anderson <aaronlynanderson@gmail.com>
2018-03-02 13:10:10 -0600

    adds freshness

--- src/layouts/home/form.js
+++ src/layouts/home/form.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
 
-let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"))
+let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"));
 let ABI = require('../../../abi/PeopleABI.js');
 let peopleAddress = '0x345ca3e014aaf5dca488057592ee47305d9b3e10';
 let People = web3.eth.contract(ABI).at(peopleAddress);
